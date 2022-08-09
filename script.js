a72483f4b8f0cb652c50c03d27edf24311b9a256
Voronovich_dev <alexanderbizv1@gmail.com>
2022-08-09 14:21:14 +0300

    code formatted

--- script.js
+++ script.js
@@ -1,10 +1,99 @@
 const btn = document.createElement('button')
-btn.innerHTML = 'Customize field'
-document.body.prepend(btn)
-btn.addEventListener('click', showPopup)
+const initialText = document.createElement("h2")
+const sizeInput = document.createElement('input')
+const sizeInputButton = document.createElement('button')
+const resetButton = document.createElement('button')
+const container = document.querySelector('#container')
 let fieldSize = 0
 
-const initialText = document.createElement("h2")
+/* ------------------------------------CONTAINER------------------------------------ */
+container.style.cssText = `
+margin: 50px auto;
+display: flex;
+width: 640px;
+flex-wrap:wrap;`
+/*----------------------------------------------------------------------------------- */
+
+
+/* --------------------------------------INPUT--------------------------------------- */
+sizeInput.type = 'number'
+sizeInput.placeholder = 'enter value from 16 to 100'
+sizeInput.style.cssText = `position: absolute;
+margin: 0 ${+document.body.clientWidth / 2 - 150}px;
+top: 2%;
+font-family: 'Josefin Sans', sans-serif;
+font-size: 18px;
+width: 300px`
+/*---------------------------------------------------------------------------------- */
+
+
+/* -----------------------------------RESET-BUTTON---------------------------------- */
+resetButton.innerHTML = 'Reset field'
+resetButton.style.cssText = `position: absolute;
+color: black;
+border: 3px #990000 solid;
+border-radius: 10px;
+left: 100px;
+top: 230px;
+font-family: 'Josefin Sans', sans-serif;
+font-size: 18px;
+background-color: #ffb3b3;
+display: flex;
+align-items: center;
+justify-content: center;
+height: 40px;
+transition: all 0.3s ease;`
+document.body.append(resetButton)
+resetButton.onmouseover = (e) => {
+   e.target.style.backgroundColor = '#ff4d4d'
+   e.target.style.transform = 'scale(1.1)'
+}
+resetButton.onmouseout = (e) => {
+   e.target.style.backgroundColor = '#ffb3b3'
+   e.target.style.transform = ''
+}
+resetButton.addEventListener('click', () => {
+   let arr = Array.from(container.children);
+   arr.forEach(el => el.style.backgroundColor = 'white')
+   fieldSize = 0
+})
+/*---------------------------------------------------------------------------------- */
+
+/* ------------------------------CUSTOMIZE-FIELD-BUTTON----------------------------- */
+btn.innerHTML = 'Customize field'
+btn.addEventListener('click', showPopup)
+function showPopup() {
+   btn.after(sizeInput)
+   sizeInput.after(sizeInputButton)
+   btn.style.display = 'none'
+   btn.removeEventListener('click', showPopup)
+}
+btn.style.cssText = `position: absolute;
+border: 3px #006600 solid;
+border-radius: 10px;
+left: 100px;
+top: 150px;
+font-family: 'Josefin Sans', sans-serif;
+font-size: 18px;
+background-color: #ccffcc;
+display: flex;
+align-items: center;
+justify-content: center;
+height: 40px;
+transition: all 0.3s ease;`
+document.body.prepend(btn)
+btn.onmouseover = (e) => {
+   e.target.style.backgroundColor = '#00b300'
+   e.target.style.transform = 'scale(1.1)'
+}
+btn.onmouseout = (e) => {
+   e.target.style.backgroundColor = '#ccffcc'
+   e.target.style.transform = ''
+}
+/*---------------------------------------------------------------------------------- */
+
+
+/* -------------------------------------INFO-TEXT----------------------------------- */
 initialText.innerHTML = `Click on the [Customize field]-button to start drawing. Then, in the pop-up window that appears on top, enter the desired size, and after that, click on the [create new field]-button that will appear on the left. If you want to clean you field click on the [Reset field]-button. Happy drawing!`
 document.body.append(initialText)
 initialText.style.cssText = `position: absolute;
@@ -14,59 +103,39 @@ font-family: 'Josefin Sans', sans-serif;
 font-size: 24px;
 text-align: center;
 width: 500px`
+/*---------------------------------------------------------------------------------- */
 
-function showPopup() {
-   btn.after(sizeInput)
-   sizeInput.after(sizeInputButton)
-   btn.style.display = 'none'
-   btn.removeEventListener('click', showPopup)
-}
-
-let sizeInput = document.createElement('input')
-sizeInput.type = 'number'
-sizeInput.placeholder = 'enter value from 16 to 100'
-let sizeInputButton = document.createElement('button')
+/* ------------------------------CREATE-NEW-FIELD-BUTTON---------------------------- */
 sizeInputButton.innerHTML = 'Create new field'
-let resetButton = document.createElement('button')
-resetButton.innerHTML = 'Reset field'
-
-document.body.append(resetButton)
-
-btn.style.cssText = `position: absolute;
+sizeInputButton.style.cssText = `position: absolute;
+border: 3px #006600 solid;
+border-radius: 10px;
 left: 100px;
-top: 150px;
-font-family: 'Josefin Sans', sans-serif;
-font-size: 18px;`
-
-resetButton.style.cssText = `position: absolute;
-left: 100px;
-top: 200px;
-font-family: 'Josefin Sans', sans-serif;
-font-size: 18px
-`
-sizeInput.style.cssText = `position: absolute;
-margin: 0 ${+document.body.clientWidth / 2 - 150}px;
 top: 2%;
 font-family: 'Josefin Sans', sans-serif;
 font-size: 18px;
-width: 300px`
-
-sizeInputButton.style.cssText = `position: absolute;
-left: 100px;
-top: 150px;
-font-family: 'Josefin Sans', sans-serif;
-font-size: 18px;`
-
-
+background-color: #ccffcc;
+display: flex;
+align-items: center;
+justify-content: center;
+height: 40px;
+transition: all 0.3s ease;`
+sizeInputButton.onmouseover = (e) => {
+   e.target.style.backgroundColor = '#00b300'
+   e.target.style.transform = 'scale(1.1)'
+}
+sizeInputButton.onmouseout = (e) => {
+   e.target.style.backgroundColor = '#ccffcc'
+   e.target.style.transform = ''
+}
 sizeInputButton.addEventListener('click', () => {
    initialText.style.display = 'none'
    if (sizeInput.value.toString() < 16 || sizeInput.value.toString() > 100 || sizeInput.value % 1 != 0) {
       alert('PLEASE, ENTER THE INTEGER VALUE FROM 16 TO 100')
       return
    }
-   btn.style.display = ''
+   btn.style.display = 'none'
    fieldSize = sizeInput.value.toString()
-   console.log(fieldSize)
    container.innerHTML = ''
    for (let i = 0; i < fieldSize * fieldSize; i++) {
       let cell = document.createElement('div')
@@ -84,21 +153,7 @@ sizeInputButton.addEventListener('click', () => {
    }
    sizeInput.value = ''
 })
-
-
-resetButton.addEventListener('click', () => {
-   let arr = Array.from(container.children);
-   arr.forEach(el => el.style.backgroundColor = 'white')
-   fieldSize = 0
-})
-
-const container = document.querySelector('#container')
-container.style.cssText = `
-margin: 50px auto;
-display: flex;
-width: 640px;
-flex-wrap:wrap;
-`
+/*---------------------------------------------------------------------------------- */
 
 
 
